0b22886b88e90011767e2e4ce57f2af619c1258f
Elijah Lawson <ElijahAtWork@gmail.com>
2023-06-07 16:33:00 -0500

    Implemented flowbite navbar

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 
-const profileRouter = require('./router/profile.router')
+const profileRouter = require('./routes/profile.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
