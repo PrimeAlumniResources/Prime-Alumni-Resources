f84a93da2a9bfa363995365fead57d249495ba48
Luis Ortiz <l.ortizintec@gmail.com>
2023-06-13 20:53:36 -0400

    saving to database works good

--- server/routes/resource.router.js
+++ server/routes/resource.router.js
@@ -13,6 +13,29 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 
 router.post('/', rejectUnauthenticated, (req, res) => {
   console.log(req.body, req.user.id);
+  const title = req.body.title
+  const link = req.body.link
+  const description = req.body.description
+  const tag = req.body.tag
+  const userId = req.user.id
+  console.log(title, link, description, tag, userId);
+ 
+  const sqlQuery = `
+  INSERT INTO "resource" 
+  ("title", "link", "description", "tag", "user_id")
+  VALUES ($1, $2, $3, $4, $5);
+  `;
+ const sqlValues = [title, link, description, tag, userId];
+ 
+ pool.query(sqlQuery, sqlValues)
+ .then((dbRes) => {
+   res.sendStatus(201);
+ })
+ .catch((dbErr) => {
+   console.log('POST /database fail:', dbErr);
+   res.sendStatus(500);
+ })
+
 });
 
 module.exports = router;
