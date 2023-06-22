72d868c3d3bd8ed8f0e76977b67c8720a900c6e7
Elijah Lawson <ElijahAtWork@gmail.com>
2023-06-22 10:26:42 -0500

    Fixed routing after login

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -48,7 +48,7 @@ function App() {
 
           <Route
             path="/login"
-            element={user.id ? <Navigate to="/user" /> : <LoginPage />}
+            element={user.id ? <Navigate to="/home" /> : <LoginPage />}
           />
 
           <Route
